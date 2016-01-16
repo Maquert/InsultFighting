9f6e991a884d1fbaa8214caa7f80e970e305f974
Miguel Hernández Jaso <miguel@mhjaso.com>
2016-01-16 18:32:31 +0100

    Initial Commit

---
+++ InsultFighting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InsultFighting
+//
+//  Created by Miguel Hernández Jaso on 16/01/16.
+//  Copyright © 2016 Miguel Hernández Jaso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
